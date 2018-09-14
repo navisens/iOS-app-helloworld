d09505fa8f81b9d557b0b75d3e0fe2c46d1528b9
James Grantham <james.grantham@navisens.com>
2018-09-14 10:17:08 -0700

    Initial Commit

---
+++ iOS-helloworld-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-helloworld-ObjC
+//
+//  Created by James Grantham on 9/14/18.
+//  Copyright © 2018 Navisens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS-helloworld-ObjC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS-helloworld-ObjC
+//
+//  Created by James Grantham on 9/14/18.
+//  Copyright © 2018 Navisens. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
