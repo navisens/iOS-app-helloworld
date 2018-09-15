affee07f08af79ebee3c9c75183d8b790dec14e1
James Grantham <james.grantham@navisens.com>
2018-09-14 17:32:16 -0700

    better logging of SDK reported errors

--- iOS-helloworld-ObjC/MotionDnaManager.m
+++ iOS-helloworld-ObjC/MotionDnaManager.m
@@ -19,7 +19,25 @@
 //    Report any errors of the estimation or internal SDK
 
 - (void)reportError:(ErrorCode)error WithMessage:(NSString *)message {
-    
+    switch (error) {
+        case SENSOR_TIMING:
+            NSLog(@"Error: Sensor Timing %@", message);
+            break;
+        case AUTHENTICATION_FAILED:
+            NSLog(@"Error: Authentication Failed %@", message);
+            break;
+        case SENSOR_MISSING:
+            NSLog(@"Error: Sensor Missing %@", message);
+            break;
+        case SDK_EXPIRED:
+            NSLog(@"Error: SDK Expired %@", message);
+            break;
+        case WRONG_FLOOR_INPUT:
+            NSLog(@"Error: Wrong Floor Input %@", message);
+            break;
+        default:
+            NSLog(@"Error: Unknown Cause");
+    };
 }
 
 - (void)receiveNetworkData:(MotionDna *)motionDna {
